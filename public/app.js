b7c987ff956b9822b514e916af8cc7fde6d8fc61
steve <stephentriphahn@yahoo.com>
2015-02-17 20:49:16 -0500

    created router, took away page reload behavior

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+//sets underscores templating delimeters to {{}}
+_.templateSettings = {
+   interpolate: /\{\{(.+?)\}\}/g
+};
+
+
+
 //the Post model and collection
 var Post = Backbone.Model.extend({});
 
@@ -10,9 +17,59 @@ var Posts = Backbone.Collection.extend({
 //the PostListView- view class for each individual post, which lists our models
 var PostListView = Backbone.View.extend({
    tagName: "li",
-   template: _.template("<a href='/posts/{{id}}'>{{title}}</a>");,
+   template: _.template("<a href='/posts/{{id}}'>{{title}}</a>"),
    render: function () {
       this.el.innerHTML = this.template(this.model.toJSON());
       return this;
    }
+   events: {
+      'click a': 'handleClick'
+   },
+   handleClick: function (e) {
+      e.preventDefault();
+      postRouter.navigate($(e.currentTarget).attr("href"),
+         {trigger: true});
+   }
 });
+
+//the PostsListView - wrapper of the PostListView instances
+var PostsListView = Backbone.View.extend({
+   template: _.template("<h1>My Blog</h1><ul></ul>"),
+   render: function () {
+      this.el.innerHTML = this.template();
+      var ul = this.$el.find("ul");
+      this.collection.forEach(function (post) {
+         ul.append(new PostListView({
+            model: post
+         }).render().el);
+      });
+      return this;
+   }
+});
+
+//Router for posts
+var PostRouter = Backbone.Router.extend({
+  initialize: function (options) {
+     this.posts = options.posts;
+     this.main = options.main;
+  },
+  routes: {
+     '': 'index',
+     'posts/:id':'singlePost'
+  },
+  index: function () {
+     var pv = new PostsListView({collection: this.posts });
+     this.main.html (pv.render().el);
+  },
+  singlePost: function (id) {
+     console.log("view post " + id);
+  }
+});
+
+
+
+
+
+
+
+
